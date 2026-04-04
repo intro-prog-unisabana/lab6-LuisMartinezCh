204f7e03aeb20a6f720ccd27f7a7de098d36b893
Luis Martinez <luismach@unisabana.edu.co>
2026-04-03 23:36:39 -0500

    lab6 afterall 3

--- dict_func.py
+++ dict_func.py
@@ -1,12 +1,11 @@
 # Write your code here!
-def employee_print(employee_info):
+def employee_print(employee_info={"Name":None, "Salary": None, "Role": None}):
     for key, value in employee_info.items():
         if value is None:
             print(f"{key}: N/A")
         else:
             print(f"{key}: {value}")
 
-    if len(employee_info) < 3:
+    if len(employee_info) <= 3:
         print("No other info!")
-    if len(employee_info) == 0:
-        print("Name: N/A")
+    
